d17c5254a08d24651c7504726e4629c5269d2dce
puwang <puwang@thoughtworks.com>
2017-07-10 23:54:14 +0800

    one CM equals one CM

---
+++ src/com/tw/Centimeter.java
@@ -0,0 +1,26 @@
+package com.tw;
+
+public class Centimeter {
+
+    private int amount;
+
+    public Centimeter(int amount) {
+
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Centimeter that = (Centimeter) o;
+
+        return amount == that.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return amount;
+    }
+}
---
+++ test/com.tw.test/CentimeterTest.java
@@ -0,0 +1,19 @@
+package com.tw.test;
+
+import com.tw.Centimeter;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class CentimeterTest {
+    @Test
+    public void one_centimeter_equals_one_centimeter() throws Exception {
+        assertEquals(new Centimeter(1), new Centimeter(1));
+    }
+
+    @Test
+    public void one_centimeter_not_equals_two_centimeter() throws Exception {
+        assertNotEquals(new Centimeter(1), new Centimeter(2));
+    }
+}
